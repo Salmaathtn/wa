1083ae415bcdcd1708cb2e5503a66557bfa8a08c
Salmaathtn <salmaathoetani@gmail.com>
2024-12-18 14:28:18 +0700

    deploy

--- db/db.js
+++ db/db.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  // connectionString: process.env.DB_URL,
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASSWORD,
+  // port: process.env.DB_PORT,
 });
 
 module.exports = pool;
